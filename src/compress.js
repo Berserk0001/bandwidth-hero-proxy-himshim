771fbede7d488d24bf834f03628d3bf9663a144a
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2023-07-17 10:10:30 +0530

    Update compress.js

--- src/compress.js
+++ src/compress.js
@@ -1,29 +1,27 @@
-const sharp = require('sharp');
-const redirect = require('./redirect');
+const sharp = require('sharp')
+const redirect = require('./redirect')
 
 function compress(req, res, input) {
-  const format = req.params.avif ? 'avif' : 'jpeg';
-  let compressionQuality = quality * 0.1;  //use const by default
+  const format = req.params.avif ? 'avif' : 'jpeg'
 
-        quality = Math.ceil(compressionQuality);
-
-	return sharp(input)
-		.grayscale(grayscale)
-		.toFormat(format, {
-			quality: quality,
-			effort: 1
+  sharp(input)
+    .grayscale(req.params.grayscale)
+    .toFormat(format, {
+      quality: req.params.quality,
+      progressive: true,
+      optimizeScans: true
     })
     .toBuffer((err, output, info) => {
-      if (err || !info || res.headersSent) {
-        return redirect(req, res);
-      }
+      if (err || !info || res.headersSent) return redirect(req, res)
 
-      res.setHeader('content-type', `image/${format}`);
-      res.setHeader('content-length', info.size);
-      res.setHeader('x-original-size', req.params.originSize);
-      res.setHeader('x-bytes-saved', req.params.originSize - info.size);
-      res.status(200).send(output);
-    });
+      res.setHeader('content-type', `image/${format}`)
+      res.setHeader('content-length', info.size)
+      res.setHeader('x-original-size', req.params.originSize)
+      res.setHeader('x-bytes-saved', req.params.originSize - info.size)
+      res.status(200)
+      res.write(output)
+      res.end()
+    })
 }
 
-module.exports = compress;
+module.exports = compress
