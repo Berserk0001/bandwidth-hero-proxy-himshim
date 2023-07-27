5bdb99229e6ca90a4590a04461244c7f36d99c81
Berserk0001 <125671174+Berserk0001@users.noreply.github.com>
2023-07-27 14:24:20 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 'use strict'
+
 const params = require('./src/params')
+
 const PORT = process.env.PORT || 8080
+
 app.get('/', params)
 app.get('/favicon.ico', (req, res) => res.status(204).end())
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
